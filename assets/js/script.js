4fc802d326f30bbf2fa8ae7bf2315825f554d754
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-05-27 22:25:44 -0400

    added modal and HTML styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,46 @@
 // variable declarations start here
-var musicSearch = function () {
-    //variable declarations end here
-    var searchArtistEl = document.querySelector("#artistEntry").value.trim();
-    var searchTrackEl = document.querySelector("#trackEntry").value.trim();
 
-    fetch(
-        'https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=' + searchArtistEl + '&track=' + searchTrackEl + '&api_key=81f35ca97ccc4666d2af29e3c5709eba&format=json&limit=30'
-    )
+$(document).ready(function(){
+    $('.modal').modal();
+  });
 
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (response) {
-            console.log(response);
+    var generatePlaylist = function () {
+        
+        document.getElementById("list").style.display = "block";
+        //variable declarations end here
+        var searchArtistEl = document.querySelector("#artistEntry").value.trim();
+        var searchTrackEl = document.querySelector("#trackEntry").value.trim();
 
-            for (i = 0; i < response.similartracks.track.length; i++) {
-                var bodyEl = document.getElementById("body");
-                var rowEl = document.createElement("tr");
-                var artistEl = document.createElement("td");
+        fetch(
+            'https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=' + searchArtistEl + '&track=' + searchTrackEl + '&api_key=81f35ca97ccc4666d2af29e3c5709eba&format=json&limit=30'
+        )
 
-                artistEl.textContent = response.similartracks.track[i].artist.name;
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (response) {
+                console.log(response);
 
-                var titleEl = document.createElement("td");
+                for (i = 0; i < response.similartracks.track.length; i++) {
+                    var bodyEl = document.getElementById("body");
+                    var rowEl = document.createElement("tr");
+                    var artistEl = document.createElement("td");
 
-                titleEl.textContent = response.similartracks.track[i].name;
-                titleEl.classList.add("col", "s4", "center-align");
-                artistEl.classList.add("col", "s4", "center-align");
+                    artistEl.textContent = response.similartracks.track[i].artist.name;
+
+                    var titleEl = document.createElement("td");
+
+                    titleEl.textContent = response.similartracks.track[i].name;
+                    titleEl.classList.add("col", "s4", "center-align");
+                    artistEl.classList.add("col", "s4", "center-align");
 
 
-                rowEl.appendChild(artistEl);
-                rowEl.appendChild(titleEl);
+                    rowEl.appendChild(artistEl);
+                    rowEl.appendChild(titleEl);
 
-                bodyEl.appendChild(rowEl);
-            }
+                    bodyEl.appendChild(rowEl);
+                }
 
-        });
-}
+            });
+    }
+
